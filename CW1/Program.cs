56d4cdf26601b605633e572778446440ee00975b
Anton <koshkai2005@gmail.com>
2026-03-10 11:40:26 +0100

    change var

--- CW1/Program.cs
+++ CW1/Program.cs
@@ -3,8 +3,8 @@
 namespace App {
     class Program {
         static void Main(string[] args) {
-            for (int i = 0; i < 5; i ++) {
-                Console.WriteLine(i);
+            for (int z = 0; z < 5; z ++) {
+                Console.WriteLine(z);
             }
         }
     }
