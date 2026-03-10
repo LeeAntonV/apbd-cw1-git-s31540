adc5dcd4475adfd9b7fff2a7235010d60211c121
Anton <koshkai2005@gmail.com>
2026-03-10 10:54:38 +0100

    Square calculation

--- CW1/Program.cs
+++ CW1/Program.cs
@@ -4,7 +4,7 @@ namespace App {
     class Program {
         static void Main(string[] args) {
             for (int i = 0; i < 5; i ++) {
-                Console.WriteLine(i);
+                Console.WriteLine(i * i);
             }
         }
     }
