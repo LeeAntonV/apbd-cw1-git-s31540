7114d470a2a851bd2976d2291f134bd308a83061
Anton <koshkai2005@gmail.com>
2026-03-10 10:53:49 +0100

    Program start message
    # Conflicts:
    #	CW1/Program.cs

--- CW1/Program.cs
+++ CW1/Program.cs
@@ -3,11 +3,9 @@
 namespace App {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
             for (int i = 0; i < 5; i ++) {
                 Console.WriteLine(i);
             }
-            Console.WriteLine("another");
         }
     }
 }
