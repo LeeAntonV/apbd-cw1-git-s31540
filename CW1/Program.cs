217d11dc9850367ce25279c5ce141dda862f70d2
Anton <koshkai2005@gmail.com>
2026-03-10 11:37:02 +0100

    changes

--- CW1/Program.cs
+++ CW1/Program.cs
@@ -1,48 +1,59 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace App {
-    class Program {
-        static void Main(string[] args) {
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
             Console.WriteLine("Printing Numbers:");
-            for (int i = 0; i < 5; i ++) {
+            for (int i = 0; i < 5; i++)
+            {
                 Console.WriteLine(i * i);
             }
+
             Console.Write("Goodbye");
         }
 
         static int CalculateAverage(int[] values)
         {
             int average = 0;
-            for (int i = 0; i < values.Length; i++){
+            for (int i = 0; i < values.Length; i++)
+            {
                 average += values[i];
             }
-            
+
             return average / values.Length;
         }
 
         static int CalculateMax(int[] values)
         {
             int max = values[0];
-            for (int i = 1; i < values.Length; i++){
-                if (values[i] > max) {
+            for (int i = 1; i < values.Length; i++)
+            {
+                if (values[i] > max)
+                {
                     max = values[i];
                 }
             }
 
             return max;
         }
-        
-        static int CalculateMin(int[] values) {
+
+        static int CalculateMin(int[] values)
+        {
             int min = values[0];
-            for (int i = 0; i < values.Length; i++){
+            for (int i = 0; i < values.Length; i++)
+            {
                 if (min > values[i])
                 {
                     min = values[i];
                 }
             }
-            
+
             return min;
+        }
     }
 }
     
