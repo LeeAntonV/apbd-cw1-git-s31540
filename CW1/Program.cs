405f631691b989beb56f8d402cf754855a29be52
Anton <koshkai2005@gmail.com>
2026-03-10 10:56:46 +0100

    loop limit input

--- CW1/Program.cs
+++ CW1/Program.cs
@@ -3,8 +3,10 @@
 namespace App {
     class Program {
         static void Main(string[] args) {
+            Console.Write("Enter numbers: ");
+            int limit = int.Parse(Console.ReadLine());
             Console.WriteLine("Printing Numbers:");
-            for (int i = 0; i < 5; i ++) {
+            for (int i = 0; i < limit; i ++) {
                 Console.WriteLine(i * i);
             }
         }
